3eef3f7e4aee84141fd6a6c6487be3965fb26b80
Alaivin <alaivin@yandex.ru>
2019-11-14 21:14:08 +0300

    Feature: created players block

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,20 @@
-//Press start game
+//Press to show the rules
+
 const startButton = document.getElementById('start');
 const mainContainer = document.getElementsByClassName('main-container')[0];
 const containerInstructions = document.getElementsByClassName('container-instructions')[0];
 startButton.addEventListener('click', event => {
-    event.preventDefault();
-    mainContainer.style.display = 'none';
-    containerInstructions.style.display = 'block';
+  event.preventDefault();
+  mainContainer.style.display = 'none';
+  containerInstructions.style.display = 'block';
 });
+
+//Press to enter players information
+
+const goGame = document.getElementById('goGame');
+const playersContainer = document.getElementsByClassName('players-container')[0];
+goGame.addEventListener('click', event => {
+  event.preventDefault();
+    containerInstructions.style.display = 'none';
+    playersContainer.style.display = 'block';
+  });
