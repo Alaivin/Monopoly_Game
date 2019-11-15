ce20033fe7c4a911265f945b98019fe88588ca5f
Alaivin <alaivin@yandex.ru>
2019-11-15 14:43:27 +0300

    Feature: added function for submit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,6 +4,10 @@ const allPlayers = {
   playerOne : {},
   playerTwo : {}
 };
+
+const boardPosition = ['go', 'goldenGatePark', 'huntersPoint', 'chance3', 'sunset', 'nobHill', 'oneWayStreet', 'noeValley', 'missionDistrict', 'chance4', 'chinaTown','haightAshbury','fog','theCastro','civicCenter','chance1', 'alamoSquare', 'dogPatch', 'jail', 'northBeach', 'soma', 'chance2', 'financialDistrict', 'russianHill','go'];
+let playerOnePositionCounter = 0;
+let playerTwoPositionCounter = 0;
 const diceAudio = document.getElementById("audioDice");
 const fogAudio = document.getElementById("audioFog");
 const jailAudio = document.getElementById("audioJail");
@@ -64,4 +68,55 @@ playerTwoDie.addEventListener('click', function() {
       this.classList.add('die');
       allPlayers.playerTwo.firstroll = startDieTwo;
   }
+});
+
+////CLick submit on the Player Info screen
+const submitInfo = document.getElementById('submitInfo');
+const playerOne = document.getElementById('playerOne');
+const playerTwo = document.getElementById('playerTwo');
+const playerOneShowName = document.getElementById('playerOneShowName');
+const playerTwoShowName = document.getElementById('playerTwoShowName');
+const playerOneGameCash = document.getElementById('playerOneGameCash');
+const playerTwoGameCash = document.getElementById('playerTwoGameCash');
+const playerOneGameDie = document.getElementById('playerOneGameDie');
+const playerOneGameDieImage = document.getElementById('playerOneGameDieImage');
+const playerTwoGameDie = document.getElementById('playerTwoGameDie');
+const playerTwoGameDieImage = document.getElementById('playerTwoGameDieImage');
+const containerMainGame = document.getElementById('containerMainGame');
+
+submitInfo.addEventListener('click', event => {
+  event.preventDefault();
+  if (playerOne.value === '' || playerTwo.value === '') {
+    errorAudio.play();
+    alert('Введите имена игроков');
+  } else if (allPlayers.playerOne.firstroll === undefined || allPlayers.playerTwo.firstroll === undefined) {
+    errorAudio.play();
+    alert('Нажмите на кости, чтобы разыграть первый ход')
+  } else {
+      allPlayers.playerOne.name = playerOne.value;
+      allPlayers.playerTwo.name = playerTwo.value;
+      allPlayers.playerOne.currentPosition = "go";
+      allPlayers.playerTwo.currentPosition = "go";
+      allPlayers.playerOne.cash = 1000;
+      allPlayers.playerTwo.cash = 1000;
+      playersContainer.style.display = 'none';
+      playerOneShowName.text = allPlayers.playerOne.name;
+      playerTwoShowName.text = allPlayers.playerTwo.name;
+      playerOneGameCash.text = allPlayers.playerOne.cash;
+      playerTwoGameCash.text = allPlayers.playerTwo.cash;
+      displayPlayerPiece('playerOne', boardPosition[playerOnePositionCounter], 'show');
+      displayPlayerPiece('playerTwo', boardPosition[playerOnePositionCounter], 'show');
+        if (playerOneDie > playerTwoDie){
+          allPlayers.playerOne.turn = true;
+          allPlayers.playerTwo.turn = false;
+          playerTwoGameDie.style.display = 'none';
+          playerTwoGameDieImage.style.display = 'none';
+      } else {
+          allPlayers.playerOne.turn = false;
+          allPlayers.playerTwo.turn = true;
+          playerOneGameDie.style.display = 'none';
+          playerOneGameDieImage.style.display = 'none';
+      }
+      containerMainGame.style.display = 'block';
+    }
 });
