674b817b7ee14e15514ec8a916c73d388c329fda
Alaivin <alaivin@yandex.ru>
2019-11-15 14:11:04 +0300

    Feature: added die roll on the Info screen

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,18 @@
+const startDieOne = Math.ceil(Math.random()*6);
+const startDieTwo = Math.ceil(Math.random()*6);
+const allPlayers = {
+  playerOne : {},
+  playerTwo : {}
+};
+const diceAudio = document.getElementById("audioDice");
+const fogAudio = document.getElementById("audioFog");
+const jailAudio = document.getElementById("audioJail");
+const errorAudio = document.getElementById("audioError");
+const getMoneyAudio = document.getElementById("audioGo");
+const buyAudio = document.getElementById("audioBuy");
+const noMoneyAudio = document.getElementById("audioNoMoney");
+const honkAudio = document.getElementById("audioHonk");
+
 //Press to show the rules
 
 const startButton = document.getElementById('start');
@@ -18,3 +33,35 @@ goGame.addEventListener('click', event => {
     containerInstructions.style.display = 'none';
     playersContainer.style.display = 'block';
   });
+
+//Player One rolls die on the Info screen
+let playerOneDie = document.getElementById('playerOneDie');
+playerOneDie.addEventListener('click', function() {
+  diceAudio.pause();
+  diceAudio.currentTime = 0;
+  if (startDieOne === startDieTwo){
+      startDieOne = Math.ceil(Math.random() * 6);
+  } else {
+      playerOneDie = startDieOne;
+      diceAudio.play();
+      this.setAttribute('src', `assets/images/dice/${startDieOne}.png`);
+      this.classList.add('die');
+      allPlayers.playerOne.firstroll = startDieOne;
+  }
+});
+
+//Player Two rolls die on the Info screen
+let playerTwoDie = document.getElementById('playerTwoDie');
+playerTwoDie.addEventListener('click', function() {
+  diceAudio.pause();
+  diceAudio.currentTime = 0;
+  if (startDieTwo === startDieOne){
+      startDieTwo = Math.ceil(Math.random() * 6);
+  } else {
+      playerTwoDie = startDieTwo;
+      diceAudio.play();
+      this.setAttribute('src', `assets/images/dice/${startDieTwo}.png`);
+      this.classList.add('die');
+      allPlayers.playerTwo.firstroll = startDieTwo;
+  }
+});
